ec64eeefbe22686bbc484fa304ea2aa427349d84
Keith Misael <95721992+xochitlcoxtoc@users.noreply.github.com>
2022-03-10 12:10:11 -0500

    Changing to PHP

---
+++ scripts/footer.js
@@ -0,0 +1,5 @@
+// Copyright
+const copyright = document.getElementById('copyright');
+const date = new Date();
+const copy_year = date.getFullYear();
+copyright.textContent = `© ${copy_year} Copyright`;
