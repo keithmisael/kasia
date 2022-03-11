ec857b11b2e678190decebacb04ef59f62d2d7bc
Keith Misael <95721992+xochitlcoxtoc@users.noreply.github.com>
2022-03-11 09:44:21 -0500

    Delete views directory

--- views/templates/confirm.php
+++
@@ -1,8 +0,0 @@
-<!-- Testimonial post form submit -->
-<div class="confirm-container">
-  <div class="confirm-img"></div>
- <div class="confirm-content">
-   <h3 class="confirm">"Best massage I have ever had. Kasia is simply amazing."</h3>
-   <p class="confirm">- a person's name</p>
-  </div>
-</div>
