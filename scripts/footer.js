c5eb13bf54773017050d52974c8101b47dfef19d
Keith Misael <95721992+xochitlcoxtoc@users.noreply.github.com>
2022-03-25 10:44:26 -0400

    Delete scripts directory

--- scripts/footer.js
+++
@@ -1,5 +0,0 @@
-// Copyright
-const copyright = document.getElementById('copyright');
-const date = new Date();
-const copy_year = date.getFullYear();
-copyright.textContent = `© ${copy_year} Copyright`;
